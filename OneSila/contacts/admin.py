6f5a30aab0928b54aa475c11c80c7caa0d4d6bca
Sascha Dobbelaere <sascha@tweave.tech>
2023-11-02 14:52:27 +0100

    add other admin pages

--- OneSila/contacts/admin.py
+++ OneSila/contacts/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from contacts.models import Company
+from contacts.models import Company, Person, Address
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Person)
+class ContactAdmin(admin.ModelAdmin):
     pass
