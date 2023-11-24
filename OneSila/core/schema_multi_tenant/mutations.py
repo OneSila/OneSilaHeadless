4ff0d39f6c25cf7c3ec8572d00f3cdcd7159f6fc
Sascha Dobbelaere <sascha@tweave.tech>
2023-11-24 13:08:42 +0000

    fix update multi-tenant-company

--- OneSila/core/schema_multi_tenant/mutations.py
+++ OneSila/core/schema_multi_tenant/mutations.py
@@ -1,9 +1,28 @@
 from strawberry_django import auth
 
-from core.schema.mutations import create, update, delete, type, List, field
+from core.schema.mutations import create, type, DjangoUpdateMutation, \
+    GetMultiTenantCompanyMixin, default_extensions, update, Info, models, \
+    Iterable, Any
+from core.schema.mixins import GetQuerysetMultiTenantMixin
+
 from .types.types import MultiTenantUserType, MultiTenantCompanyType
 from .types.input import MultiTenantUserInput, MultiTenantUserPartialInput, \
     MultiTenantCompanyPartialInput, MultiTenantCompanyInput
+
+
+class MultiTentantCompanyUpdateMutation(GetMultiTenantCompanyMixin, DjangoUpdateMutation):
+    """
+    This mutation will only protect against having a multi-tentant company.
+    Not the existance on the model, since the MultiTenantCompany has no reference to itself.
+    """
+
+    def update(self, info: Info, instance: models.Model | Iterable[models.Model], data: dict[str, Any],):
+        return super().update(info=info, instance=instance, data=data)
+
+
+def update_multi_company(input_type):
+    extensions = default_extensions
+    return MultiTentantCompanyUpdateMutation(input_type, extensions=extensions)
 
 
 @type(name="Mutation")
@@ -17,4 +36,4 @@ class MultiTenantMutation:
     update_user: MultiTenantUserType = update(MultiTenantUserPartialInput)
 
     # FIXME: You should only be able to update your own multi tenant company. Not other users. (unless it's to disable)
-    update_multi_tenant_company: MultiTenantCompanyType = update(MultiTenantCompanyPartialInput)
+    update_multi_tenant_company: MultiTenantCompanyType = update_multi_company(MultiTenantCompanyPartialInput)
