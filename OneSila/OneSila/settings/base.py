bc8dc1190bd68b0083643f18212f41885f6db45e
Sascha Dobbelaere <sascha@tweave.tech>
2023-11-27 22:17:34 +0000

    adding redis

--- OneSila/OneSila/settings/base.py
+++ OneSila/OneSila/settings/base.py
@@ -164,7 +164,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [(os.getenv('REDIS_HOST', "127.0.0.1"), os.getenv('REDIS_PORT', 6379))],
         },
     },
 }
