5e870d528fb43172341fae3aa33d082ae5f53cb9
Sascha Dobbelaere <sascha@tweave.tech>
2023-11-28 15:28:57 +0000

    language adjust query for languages

--- OneSila/core/schema/languages/queries.py
+++ OneSila/core/schema/languages/queries.py
@@ -9,11 +9,17 @@ from typing import List
 
 from core.countries import COUNTRY_CHOICES
 from core.schema.languages.types.types import LanguageType
+from core.schema.core.helpers import get_current_user
 from django.conf import settings
+from django.utils.translation import activate, get_language_info, deactivate
 
 
-def get_languages() -> List[LanguageType]:
-    return [LanguageType(iso=iso, name=name) for iso, name in settings.LANGUAGES]
+def get_languages(info) -> List[LanguageType]:
+    user = get_current_user(info)
+    activate(user.language)
+    languages = [LanguageType(iso=iso, name=name) for iso, name in settings.LANGUAGES]
+    deactivate()
+    return languages
 
 
 def get_default_language_code() -> str:
