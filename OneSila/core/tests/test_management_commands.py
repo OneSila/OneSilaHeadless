4b7dbcfac346410522ba6f7c8dfefc6f4d9e0e7c
Sascha Dobbelaere <sascha@tweave.tech>
2023-12-16 16:36:18 +0000

    Add the items

--- OneSila/core/tests/test_management_commands.py
+++ OneSila/core/tests/test_management_commands.py
@@ -4,10 +4,10 @@ import os
 
 class UrlManagementCommandsTestCase(TestCase):
     def test_generate_url(self):
-        os.subprocess('./manage.py generate_urls all')
+        os.system('./manage.py generate_urls all')
 
     def test_generate_views(self):
-        os.subprocess('./manage.py generate_views all')
+        os.system('./manage.py generate_views all')
 
     def test_list_urls(self):
-        os.subprocess('./manage.py list_urls')
+        os.system('./manage.py list_urls')
