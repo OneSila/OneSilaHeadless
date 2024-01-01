b400c239552f7b6c4c67e4e98c0b19e77f5820e2
Sascha Dobbelaere <sascha@tweave.tech>
2024-01-01 20:38:59 +0000

    adding missing search fields

--- OneSila/inventory/models.py
+++ OneSila/inventory/models.py
@@ -14,6 +14,7 @@ class Inventory(models.Model):
     objects = InventorykManager()
 
     class Meta:
+        search_terms = ['product__sku', 'stocklocation__name']
         unique_together = ('product', 'stocklocation')
         verbose_name_plural = "inventories"
 
