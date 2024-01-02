9e7b3efc7941e29bf0ce9ca9aae398f26553e754
SD <sascha.dobbelaere@gmail.com>
2024-01-02 11:13:56 +0000

    fix image model (#11)
    Resolve the missing urls field and merge the validators with the core validators.
    
    ---
    Co-authored-by: Sascha Dobbelaere <sascha@tweave.tech>

--- OneSila/core/tests/__init__.py
+++ OneSila/core/tests/__init__.py
@@ -1,9 +1,14 @@
 from core.models import MultiTenantCompany
 from django.contrib.auth import get_user_model
 from model_bakery import baker
+from django.test import TestCase as DjangoTestCase
 
 
 class TestCaseMixin:
     def setUp(self):
         self.multi_tenant_company = baker.make(MultiTenantCompany)
         self.user = baker.make(get_user_model(), multi_tenant_company=self.multi_tenant_company)
+
+
+class TestCase(TestCaseMixin, DjangoTestCase):
+    pass
--- OneSila/media/helpers.py
+++ OneSila/media/helpers.py
@@ -15,15 +15,6 @@ def sanitize_media_filename(filename):
     return filename
 
 
-def get_media_folder_upload_path(instance, filename):
-    '''Return a dynmic path based on the selection location.  And clean the filename'''
-    path = os.path.join('media_files/', instance.image_location.path, sanitize_media_filename(filename))
-
-    logger.debug("Upload path {}".format(path))
-
-    return path
-
-
 def is_landscape(w, h):
     '''
     Returns True if image is landscape
