59261c56587cca225f124d5f105f74673cb0cd4d
Sascha Dobbelaere <sd@tweave.tech>
2024-03-20 18:32:38 +0000

    remove the demo_data app, in favour of registry based solution

--- OneSila/demo_data/README.md
+++
@@ -1,6 +0,0 @@
-# Demo Data
-
-This package is responsible for generating the demo-data for a given multi-company.
-
-
-## Usage
--- OneSila/demo_data/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class DemoDataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'demo_data'
-
-    def ready(self):
-        from . import receivers
