9871f2b41f1d03eb6e5e177a83c4c733bce4fc82
Sascha Dobbelaere <sd@tweave.tech>
2024-03-25 13:44:19 +0000

    convert from Tax to VatRate

--- OneSila/taxes/models.py
+++ OneSila/taxes/models.py
@@ -2,7 +2,7 @@ from core import models
 from django.utils.translation import gettext_lazy as _
 
 
-class Tax(models.Model):
+class VatRate(models.Model):
     '''
     Tax value class to assign taxes to products.  Will most likely
     only contain one value.  But should be here none the less.
@@ -10,8 +10,8 @@ class Tax(models.Model):
     These are not to be confused with the international rates one should charge eg in the euro-zone.
     although it is someting that needs to be concidered just the same: TODO
     '''
-    rate = models.IntegerField(help_text=_("Tax rate in percent.  Eg 21 for 21%"))
-    name = models.CharField(max_length=3, blank=True, null=True)
+    name = models.CharField(max_length=3, null=True)
+    rate = models.IntegerField(help_text=_("VAT rate in percent.  Eg 21 for 21%"))
 
     def __str__(self):
         return self.name
@@ -21,5 +21,5 @@ class Tax(models.Model):
         return super().save(*args, **kwargs)
 
     class Meta:
-        verbose_name = 'Tax'
-        verbose_name_plural = 'Taxes'
+        verbose_name = 'VAT Rate'
+        verbose_name_plural = 'VAT Taxes'
