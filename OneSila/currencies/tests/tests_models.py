9f84951623cf6e15c7a61e783b2624a11a3856a3
Sascha Dobbelaere <sd@tweave.tech>
2024-04-07 17:09:03 +0100

    restrict the is_default_currency only to the multi_tenant_company field

--- OneSila/currencies/tests/tests_models.py
+++ OneSila/currencies/tests/tests_models.py
@@ -18,13 +18,13 @@ class CurrencyTestCase(TestCase):
         currency_mtc = baker.make(Currency, iso_code="GBP", is_default_currency=True, follow_official_rate=True, multi_tenant_company=mtc)
 
         try:
-            currency_mtc_two = baker.make(Currency, iso_code="EUR", is_default_currency=True, follow_official_rate=True, multi_tenant_company=mtc)
-            self.fail("You should not be able to create two default currencies under the same multi_tenant_company")
-        except IntegrityError:
-            pass
-
-        try:
             currency_mtc_bis = baker.make(Currency, iso_code="GBP", is_default_currency=True, follow_official_rate=True, multi_tenant_company=mtc_bis)
         except IntegrityError:
             self.fail("You should be able to have a default currency for every multi_tenant_company")
             raise
+
+        try:
+            currency_mtc_two = baker.make(Currency, iso_code="EUR", is_default_currency=True, follow_official_rate=True, multi_tenant_company=mtc)
+            self.fail("You should not be able to create two default currencies under the same multi_tenant_company")
+        except IntegrityError:
+            pass
