fbeb07ebe08b29075af37a901d393c61336a4646
Sascha Dobbelaere <sd@tweave.tech>
2024-04-13 11:32:40 +0100

    remove unit demo data

--- OneSila/units/demo_data.py
+++ OneSila/units/demo_data.py
@@ -5,19 +5,20 @@ from units.models import Unit
 registry = DemoDataLibrary()
 
 
-@fake.add_provider
-class UnitProvider(BaseProvider):
-    def unit(self) -> str:
-        return 'piece'
+# FIXME: This provider doesnt supply the correct data.
+# @fake.add_provider
+# class UnitProvider(BaseProvider):
+#     def unit(self) -> str:
+#         return 'piece'
 
 
-class UnitGenerator(PrivateDataGenerator):
-    model = Unit
-    count = 1
-    field_mapper = {
-        'name': fake.unit,
-        'unit': fake.unit,
-    }
+# class UnitGenerator(PrivateDataGenerator):
+#     model = Unit
+#     count = 1
+#     field_mapper = {
+#         'name': fake.unit,
+#         'unit': fake.unit,
+#     }
 
 
-registry.register_private_app(UnitGenerator)
+# registry.register_private_app(UnitGenerator)
