d681ff29ec9c93337cf3eadfa35fc0def83547ec
BranDavidSebastian <brandavid1998@gmail.com>
2024-06-11 01:55:27 +0300

    #124 finish onboarding (without demo data)

--- OneSila/core/migrations/0096_alter_multitenantcompany_language_and_more.py
+++ OneSila/core/migrations/0096_alter_multitenantcompany_language_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0091_alter_multitenantuser_timezone'),
+        ('core', '0095_alter_multitenantuser_timezone'),
     ]
 
     operations = [
