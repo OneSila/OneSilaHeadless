b6b6f029a56eb639eee65b17d7f9b5406d5c6172
Sascha Dobbelaere <sd@tweave.tech>
2024-07-22 10:35:03 +0100

    #229 - move default timezone to timezone file

--- OneSila/core/timezones.py
+++ OneSila/core/timezones.py
@@ -2,3 +2,4 @@ from pytz import common_timezones_set
 
 # https://stackoverflow.com/a/13867319/5731101
 TIMEZONE_CHOICES = sorted([(i, i) for i in common_timezones_set])
+DEFAULT_TIMEZONE = 'Europe/London'
