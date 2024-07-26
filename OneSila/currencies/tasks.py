58a799ea8be87dd37227c5d3355b1f1465d7eeed
Sascha Dobbelaere <sd@tweave.tech>
2024-07-26 15:24:30 +0100

    #209 - wrongly configured task fix

--- OneSila/currencies/tasks.py
+++ OneSila/currencies/tasks.py
@@ -4,6 +4,6 @@ from huey.contrib.djhuey import db_task, db_periodic_task
 from currencies.flows import update_rate_flow
 
 
-@db_task(cronjob(day="*", hour=1, min=1))
+@db_periodic_task(crontab(day="*", hour=1, min=1))
 def currencies__currency__update_rates__cronjob():
     return update_rate_flow()
