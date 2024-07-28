cb4c2d9d55c448c5a38dc099d359b55c2aa2eaf5
Sascha Dobbelaere <sd@tweave.tech>
2024-07-28 09:18:26 +0100

    #208 - Adding comments how the inventory stucture should work

--- OneSila/inventory/managers.py
+++ OneSila/inventory/managers.py
@@ -1,10 +1,38 @@
 from core import models
 from core.models import Sum
-from core.managers import  MultiTenantQuerySet, MultiTenantManager
+from core.managers import MultiTenantQuerySet, MultiTenantManager
 from orders.models import Order
 
 import logging
 logger = logging.getLogger(__name__)
+
+
+"""
+Here is the thing, Supplier- and Manufacturable products both
+have stock directly assigned to them.
+
+Supplier-products are always 'hidden'.  Used for sums of inventory.
+But Manufacturable products are directly assinged to websites or to bundles.
+Also do we calculate how much "can" be made based on the bill of materials.
+
+ok - so let's split his up
+1. Supplier product = direct stock
+2. Simple Product = Sum of supplier product stock
+3. DropShop product = Sum of supplier product stock, or always for sales
+3. Config Product = No stock
+4. Bundle Product = Sum of the simple product, dropship product, manufacturable product and bundle product stock
+5. Manufacturable = Direct stock + Manufacturable stock.  (Lead times could be a challenge here)
+
+Do we also want access to the inventory per location?
+It seems we may need to eg the paperweight.ie example where he has internal and external stock.
+
+How to we need the stock information:
+- Actual number
+- Lead time for this number (lowest?)
+
+- Number per location
+- Lead time per location
+"""
 
 
 class InventoryQuerySet(MultiTenantQuerySet):
