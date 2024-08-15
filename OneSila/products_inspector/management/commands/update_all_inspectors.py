66acfe1d9c4245cdb1221fa9be4ca578a1c03142
BranDavidSebastian <brandavid1998@gmail.com>
2024-08-15 17:59:45 +0300

    #169 update command to also run inspector sync after created

--- OneSila/products_inspector/management/commands/update_all_inspectors.py
+++ OneSila/products_inspector/management/commands/update_all_inspectors.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from products.models import Product
 from products_inspector.factories.inspector import InspectorCreateOrUpdateFactory
+from products_inspector.flows.inspector import resync_inspector_flow
+
 
 class Command(BaseCommand):
     help = 'Update all inspectors with clear_blocks=True and run_sync=False'
@@ -14,6 +16,10 @@ class Command(BaseCommand):
         for index, product in enumerate(products, start=1):
             factory = InspectorCreateOrUpdateFactory(product, clear_blocks=True, run_sync=False)
             factory.run()
+
+            inspector = factory.inspector
+            resync_inspector_flow(inspector, run_async=False)
+
             self.stdout.write(self.style.SUCCESS(f'[{index}/{total_products}] Updated inspector for product {product.sku}'))
 
         self.stdout.write(self.style.SUCCESS('All inspectors updated successfully.'))
