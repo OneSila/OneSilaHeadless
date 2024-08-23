cae761a317612bbba17bdcb554c94ce7af4f4c97
Sascha Dobbelaere <sd@tweave.tech>
2024-08-23 01:21:05 +0100

    #255 - Add attachments and missing FK

--- OneSila/order_returns/models.py
+++ OneSila/order_returns/models.py
@@ -1,4 +1,6 @@
 from core import models
+from core.validators import validate_attachment_extensions, no_dots_in_filename
+from .helpers import get_orderreturn_attachment_folder_upload_path
 from django.utils.translation import gettext_lazy as _
 
 
@@ -26,7 +28,15 @@ class OrderReturn(models.Model):
     return_reason = models.TextField(null=True, blank=True)
 
 
+class OrderReturnAttachment(models.Model):
+    orderreturn = models.ForeignKey(OrderReturn, on_delete=models.CASCADE)
+    file = models.FileField(_('File'),
+        upload_to=get_orderreturn_attachment_folder_upload_path, validators=[validate_attachment_extensions, no_dots_in_filename],
+        null=True, blank=True)
+
+
 class OrderReturnItem(models.Model):
+    orderreturn = models.ForeignKey(OrderReturn, on_delete=models.CASCADE)
     # NOTE: Must be limited by the items sold in the related order.
     orderitem = models.ForeignKey('orders.OrderItem', on_delete=models.CASCADE)
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
