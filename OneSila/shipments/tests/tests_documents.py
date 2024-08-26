3a5ea112f30be5fc6ac3e4cb867557a203575156
Sascha Dobbelaere <sd@tweave.tech>
2024-08-26 11:04:10 +0100

    #274 - allow shipments to come from demo-data

--- OneSila/shipments/tests/tests_documents.py
+++ OneSila/shipments/tests/tests_documents.py
@@ -1,6 +1,6 @@
 from django.urls import reverse
 from core.tests import TestCaseDemoDataMixin, TestCase
-from shipments.flows import prepare_shipments_flow
+from shipments.models import Shipment
 from orders.tests.tests_factories.mixins import CreateTestOrderMixin
 from products.models import Product
 from products.demo_data import SIMPLE_PEN_SKU
@@ -12,14 +12,15 @@ logger = logging.getLogger(__name__)
 
 class PickingListDocumentTestCase(CreateTestOrderMixin, TestCaseDemoDataMixin, TestCase):
     def test_picking_list(self):
-        product = Product.objects.get(sku=SIMPLE_PEN_SKU, multi_tenant_company=self.multi_tenant_company)
-        order_qty = 1
-        order = self.create_test_order('test_prepare_shipment', product, order_qty)
-        order.set_status_pending_processing()
+        # product = Product.objects.get(sku=SIMPLE_PEN_SKU, multi_tenant_company=self.multi_tenant_company)
+        # order_qty = 1
+        # order = self.create_test_order('test_prepare_shipment', product, order_qty)
+        # order.set_status_pending_processing()
+        # self.assertEqual(order.status, order.SHIPPED)
 
-        self.assertEqual(order.status, order.SHIPPED)
+        shipment = Shipment.objects.last()
 
-        shipment = order.shipment_set.all().last()
+        # shipment = order.shipment_set.all().last()
         filename, pdf = shipment.print()
         filepath = save_test_file(filename, pdf)
 
