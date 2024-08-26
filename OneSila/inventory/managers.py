51fb9062528bc3d36b38a197e6a5667f3071fd85
BranDavidSebastian <brandavid1998@gmail.com>
2024-08-26 11:28:50 +0300

    #206 shipments

--- OneSila/inventory/managers.py
+++ OneSila/inventory/managers.py
@@ -238,7 +238,7 @@ class InventoryLocationQuerySet(MultiTenantQuerySet):
     def filter_locations_for_product(self, product):
         # this all needs tranlating into either manufacturable or supplier products to get the right locations.
         if product.is_manufacturable() or product.is_supplier_product():
-            return self.filter(multi_tenant_company=multi_tenant_company,
+            return self.filter(multi_tenant_company=product.multi_tenant_company,
                 product=product)
         elif product.is_simple():
             products = product.supplier_products.all()
@@ -253,7 +253,7 @@ class InventoryLocationQuerySet(MultiTenantQuerySet):
 
             return self.filter(id__in=location_ids)
         else:
-            raise ValueError(f"Product type {produt.type} not implemented.")
+            raise ValueError(f"Product type {product.type} not implemented.")
 
 
 class InventoryLocationManager(MultiTenantManager):
@@ -264,5 +264,5 @@ class InventoryLocationManager(MultiTenantManager):
     def filter_by_shippingaddress_set(self, shippingaddress_set):
         return self.get_queryset().filter_by_shippingaddress_set(shippingaddress_set)
 
-    def filter_locations_for_product(product, qty):
+    def filter_locations_for_product(self, product, qty):
         return self.get_queryset().filter_locations_for_product(product, qty)
