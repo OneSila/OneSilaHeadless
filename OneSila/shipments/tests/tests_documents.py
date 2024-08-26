1d7550852e9bfa87bb1ee3469f71944daef843a1
Sascha Dobbelaere <sd@tweave.tech>
2024-08-26 10:48:11 +0100

    #207 - Set a different sku with larger stock. Does this take?

--- OneSila/shipments/tests/tests_documents.py
+++ OneSila/shipments/tests/tests_documents.py
@@ -3,7 +3,7 @@ from core.tests import TestCaseDemoDataMixin, TestCase
 from shipments.flows import prepare_shipments_flow
 from orders.tests.tests_factories.mixins import CreateTestOrderMixin
 from products.models import Product
-from products.demo_data import SIMPLE_BLACK_FABRIC_PRODUCT_SKU
+from products.demo_data import SIMPLE_PEN_SKU
 from core.helpers import save_test_file
 
 import logging
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 class PickingListDocumentTestCase(CreateTestOrderMixin, TestCaseDemoDataMixin, TestCase):
     def test_picking_list(self):
-        product = Product.objects.get(sku=SIMPLE_BLACK_FABRIC_PRODUCT_SKU, multi_tenant_company=self.multi_tenant_company)
+        product = Product.objects.get(sku=SIMPLE_PEN_SKU, multi_tenant_company=self.multi_tenant_company)
         order_qty = 1
         order = self.create_test_order('test_prepare_shipment', product, order_qty)
         order.set_status_pending_processing()
