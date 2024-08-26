4cc9dee9f0a0084e4665424a050d5c5e28f756f4
Sascha Dobbelaere <sd@tweave.tech>
2024-08-26 12:04:41 +0100

    #274 - ensure tasks happen immediatly even in testsy

--- OneSila/OneSila/settings/base.py
+++ OneSila/OneSila/settings/base.py
@@ -224,7 +224,7 @@ HUEY = {
     'name': 'hueyonesilaheadless',  # Use db name for huey.
     'results': True,  # Store return values of tasks.
     'store_none': False,  # If a task returns None, do not save to results.
-    'immediate': DEBUG,  # If DEBUG=True, run synchronously.
+    'immediate': True,  # If DEBUG=True, run synchronously.
     'utc': True,  # Use UTC for all times internally.
     'blocking': True,  # Perform blocking pop rather than poll Redis.
     'connection': {
