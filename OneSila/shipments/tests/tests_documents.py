f9084278c0c7cb01d013256ba44633ebbf9fd965
Sascha Dobbelaere <sd@tweave.tech>
2024-08-26 11:42:15 +0100

    #274 - changing back to orignal data

--- OneSila/shipments/tests/tests_documents.py
+++ OneSila/shipments/tests/tests_documents.py
@@ -12,7 +12,14 @@ logger = logging.getLogger(__name__)
 
 class PickingListDocumentTestCase(CreateTestOrderMixin, TestCaseDemoDataMixin, TestCase):
     def test_picking_list(self):
+        product = Product.objects.get(sku=SIMPLE_PEN_SKU, multi_tenant_company=self.multi_tenant_company)
+        order_qty = 1
+        order = self.create_test_order('test_prepare_shipment', product, order_qty)
+        order.set_status_pending_processing()
+        self.assertEqual(order.status, order.SHIPPED)
+
         shipment = Shipment.objects.last()
+
         filename, pdf = shipment.print()
         filepath = save_test_file(filename, pdf)
 
