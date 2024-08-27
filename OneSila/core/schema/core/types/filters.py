9c2f8934cf7e62b2483b6b0b0025d525e8dc1993
Sascha Dobbelaere <sd@tweave.tech>
2024-08-27 10:47:27 +0100

    #126 - search filter is failing

--- OneSila/core/schema/core/types/filters.py
+++ OneSila/core/schema/core/types/filters.py
@@ -1,18 +1,26 @@
 from typing import Optional
 
 from strawberry_django.filters import filter as strawberry_filter
+from strawberry_django import filter_field
 from strawberry import UNSET
 from strawberry import LazyType as lazy
+from core.managers import QuerySet
 
 
 class SearchFilterMixin:
     search: str | None
 
-    def filter_search(self, queryset) -> str | None:
-        if self.search not in (None, UNSET):
-            queryset = queryset.search(self.search)
+    # def filter_search(self, queryset) -> str | None:
+    #     if self.search not in (None, UNSET):
+    #         queryset = queryset.search(self.search)
 
-        return queryset
+    #     return queryset
+
+    @filter_field()
+    def search(self, queryset: QuerySet, value: str, prefix: str) -> tuple:
+        # FIXME: Looks like "search" needs and async version called asearch as well.
+        return queryset.search(value)
+        # return queryset, Q(**{"_fullname": value})
 
 
 class ExcluideDemoDataFilterMixin:
