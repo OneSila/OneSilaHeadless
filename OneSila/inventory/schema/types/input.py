d35d1edb67f7c242221584c1e14201bf0402c866
Sascha Dobbelaere <sd@tweave.tech>
2024-08-31 10:25:43 +0100

    #126 - disable objects and fields that should not be set/created with inventory movements

--- OneSila/inventory/schema/types/input.py
+++ OneSila/inventory/schema/types/input.py
@@ -4,12 +4,17 @@ from core.schema.core.types.input import NodeInput, input, partial
 from inventory.models import Inventory, InventoryLocation
 
 
-@input(Inventory, fields="__all__")
+# Quantity is disabled as we never set quantity directly.
+# You must add an InventoryMovement from or to this Inventory.
+@input(Inventory, exclude=['quantity'])
 class InventoryInput:
     pass
 
+# Quantity is disabled as we never set quantity directly.
+# You must add an InventoryMovement from or to this Inventory.
 
-@partial(Inventory, fields="__all__")
+
+@partial(Inventory, exclude=['quantity'])
 class InventoryPartialInput(NodeInput):
     pass
 
