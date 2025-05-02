b6dd033a85b4a03aaad671697e282c462b4bc237
david <brandavid1998@gmail.com>
2025-05-02 21:27:14 +0300

    hotfix inspector issue

--- OneSila/products/models.py
+++ OneSila/products/models.py
@@ -158,58 +158,73 @@ class Product(TranslatedModelMixin, models.Model):
     def get_configurator_properties(self, product_rule=None, public_information_only=True):
         from properties.models import ProductPropertiesRuleItem
 
-        return ProductPropertiesRuleItem.objects.filter(
+        queryset = ProductPropertiesRuleItem.objects.filter(
             multi_tenant_company=self.multi_tenant_company,
-            property__is_public_information=public_information_only,
             rule=product_rule or self.get_product_rule(),
             type__in=[
                 ProductPropertiesRuleItem.REQUIRED_IN_CONFIGURATOR,
                 ProductPropertiesRuleItem.OPTIONAL_IN_CONFIGURATOR
             ]
-        ).select_related('property')
+        )
+        if public_information_only:
+            queryset = queryset.filter(property__is_public_information=True)
+
+        return queryset.select_related('property')
 
     def get_optional_in_configurator_properties(self, product_rule=None, public_information_only=True):
         from properties.models import ProductPropertiesRuleItem
 
-        return ProductPropertiesRuleItem.objects.filter(
+        queryset = ProductPropertiesRuleItem.objects.filter(
             multi_tenant_company=self.multi_tenant_company,
-            property__is_public_information=public_information_only,
             rule=product_rule or self.get_product_rule(),
             type=ProductPropertiesRuleItem.OPTIONAL_IN_CONFIGURATOR
-        ).select_related('property')
+        )
+        if public_information_only:
+            queryset = queryset.filter(property__is_public_information=True)
+
+        return queryset.select_related('property')
 
     def get_required_properties(self, product_rule=None, public_information_only=True):
         from properties.models import ProductPropertiesRuleItem
 
-        return ProductPropertiesRuleItem.objects.filter(
+        queryset = ProductPropertiesRuleItem.objects.filter(
             multi_tenant_company=self.multi_tenant_company,
-            property__is_public_information=public_information_only,
             rule=product_rule or self.get_product_rule(),
             type__in=[
                 ProductPropertiesRuleItem.REQUIRED,
                 ProductPropertiesRuleItem.REQUIRED_IN_CONFIGURATOR,
                 ProductPropertiesRuleItem.OPTIONAL_IN_CONFIGURATOR
             ]
-        ).select_related('property')
+        )
+        if public_information_only:
+            queryset = queryset.filter(property__is_public_information=True)
+
+        return queryset.select_related('property')
 
     def get_optional_properties(self, product_rule=None, public_information_only=True):
         from properties.models import ProductPropertiesRuleItem
 
-        return ProductPropertiesRuleItem.objects.filter(
+        queryset = ProductPropertiesRuleItem.objects.filter(
             multi_tenant_company=self.multi_tenant_company,
-            property__is_public_information=public_information_only,
             rule=product_rule or self.get_product_rule(),
             type=ProductPropertiesRuleItem.OPTIONAL
-        ).select_related('property')
+        )
+        if public_information_only:
+            queryset = queryset.filter(property__is_public_information=True)
+
+        return queryset.select_related('property')
 
     def get_required_and_optional_properties(self, product_rule=None, public_information_only=True):
         from properties.models import ProductPropertiesRuleItem
 
-        return ProductPropertiesRuleItem.objects.filter(
+        queryset = ProductPropertiesRuleItem.objects.filter(
             multi_tenant_company=self.multi_tenant_company,
-            property__is_public_information=public_information_only,
             rule=product_rule or self.get_product_rule(),
-        ).select_related('property')
+        )
+        if public_information_only:
+            queryset = queryset.filter(property__is_public_information=True)
+
+        return queryset.select_related('property')
 
     def get_unique_configurable_variations(self):
         from properties.models import ProductProperty
