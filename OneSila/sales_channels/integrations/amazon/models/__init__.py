e2a1f7888e79790f96d8037a18c1914af17ce3bd
David Sebastian Bran <50082813+BranDavidSebastian@users.noreply.github.com>
2025-06-25 14:52:14 +0300

    Add Amazon product import skeleton

--- OneSila/sales_channels/integrations/amazon/models/__init__.py
+++ OneSila/sales_channels/integrations/amazon/models/__init__.py
@@ -8,7 +8,10 @@ from .properties import (
     AmazonProperty, AmazonPropertySelectValue, AmazonProductProperty, AmazonProductType, AmazonProductTypeItem
 )
 from .sales_channels import (
-    AmazonSalesChannel, AmazonSalesChannelView, AmazonRemoteLanguage
+    AmazonSalesChannel,
+    AmazonSalesChannelView,
+    AmazonRemoteLanguage,
+    AmazonSalesChannelViewAssign,
 )
 from .taxes import AmazonCurrency, AmazonVat
 from .imports import *
