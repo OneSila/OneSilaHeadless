8f6f12802727887fcc701c6d388d5ef9cefacb01
david <brandavid1998@gmail.com>
2025-07-25 19:34:57 +0300

    init ebay

--- OneSila/sales_channels/integrations/amazon/factories/mixins.py
+++ OneSila/sales_channels/integrations/amazon/factories/mixins.py
@@ -386,7 +386,6 @@ class GetAmazonAPIMixin:
         if current_attributes is None or current_attributes == {}:
             current_attributes = self.get_listing_attributes(sku, marketplace_id)
 
-        print(current_attributes)
         patches = self._build_patches(current_attributes, new_attributes)
 
         body = {
