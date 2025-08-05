8537b8082ce82982245712ab7c384a6daea87814
david <brandavid1998@gmail.com>
2025-08-05 15:10:52 +0300

    remove remote_task from async importer

--- OneSila/sales_channels/integrations/amazon/tasks.py
+++ OneSila/sales_channels/integrations/amazon/tasks.py
@@ -4,7 +4,6 @@ from sales_channels.decorators import remote_task
 from core.decorators import run_task_after_commit
 
 
-@remote_task(priority=LOW_PRIORITY)
 @db_task()
 def amazon_import_db_task(import_process, sales_channel):
     from sales_channels.integrations.amazon.factories.imports.schema_imports import AmazonSchemaImportProcessor
@@ -21,7 +20,6 @@ def amazon_import_db_task(import_process, sales_channel):
         fac.run()
 
 
-@remote_task(priority=HIGH_PRIORITY)
 @db_task()
 def amazon_product_import_item_task(
     import_process_id, sales_channel_id, product_data, is_last=False, updated_with=None
