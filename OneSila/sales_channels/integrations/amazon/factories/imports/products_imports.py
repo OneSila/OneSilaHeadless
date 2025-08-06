7747d935e04c03d0e534d1062962fe82c604a399
Sascha Dobbelaere <sd@tweave.tech>
2025-08-06 10:15:50 +0200

    useless logentry, just return the seller-id, not the marketplace id.

--- OneSila/sales_channels/integrations/amazon/factories/imports/products_imports.py
+++ OneSila/sales_channels/integrations/amazon/factories/imports/products_imports.py
@@ -781,8 +781,6 @@ class AmazonProductItemFactory(AmazonProductsImportProcessor):
 
     @timeit_and_log(logger, "AmazonProductItemFactory.run")
     def run(self):
-        logger.debug(F"AmazonProductItemFactory.run - channel: {self.sales_channel.remote_id}")
-
         try:
             self.process_product_item(self.product_data)
         except Exception as exc:  # capture unexpected errors
