62d5bc1692169d6967a165490a0d54f53476cb40
David Sebastian Bran <50082813+BranDavidSebastian@users.noreply.github.com>
2025-08-28 16:58:35 +0300

    Add mutation to regenerate webhook secret

--- OneSila/webhooks/tests.py
+++ OneSila/webhooks/tests.py
@@ -1,8 +1,9 @@
 from django.core.exceptions import ValidationError
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
 
 from core.models import MultiTenantCompany
 from .models import WebhookIntegration
+from core.tests.tests_schemas.tests_queries import TransactionTestCaseMixin
 
 
 class WebhookIntegrationModelTests(TestCase):
@@ -30,3 +31,29 @@ class WebhookIntegrationModelTests(TestCase):
         )
         integration.save(force_save=True)
         self.assertIsNotNone(integration.id)
+
+
+class WebhookIntegrationMutationTests(TransactionTestCaseMixin, TransactionTestCase):
+    def test_regenerate_secret(self):
+        integration = WebhookIntegration.objects.create(
+            multi_tenant_company=self.multi_tenant_company,
+            hostname="https://example.com",
+            topic="product",
+            url="https://webhook.example.com",
+        )
+        old_secret = integration.secret
+        mutation = """
+            mutation($id: GlobalID!){
+              regenerateWebhookIntegrationSecret(instance: {id: $id}){
+                secret
+              }
+            }
+        """
+        resp = self.strawberry_test_client(
+            query=mutation,
+            variables={"id": self.to_global_id(integration)},
+            asserts_errors=False,
+        )
+        self.assertTrue(resp.errors is None)
+        new_secret = resp.data["regenerateWebhookIntegrationSecret"]["secret"]
+        self.assertNotEqual(old_secret, new_secret)
