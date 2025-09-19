35cc9c06824500b3187b89370f28acd7a6a9cb23
David Sebastian Bran <50082813+BranDavidSebastian@users.noreply.github.com>
2025-09-19 12:36:24 +0300

    Fix dependent mapped locally filter mixin

--- OneSila/sales_channels/schema/types/filter_mixins.py
+++ OneSila/sales_channels/schema/types/filter_mixins.py
@@ -10,23 +10,26 @@ from strawberry_django import filter_field as custom_filter
 from core.schema.core.types.filters import AnnotationMergerMixin
 
 
-class DependentMappedLocallyFilterMixin:
-    # @TODO: This is broken
-    pass
-    # mapped_locally_querysets: Iterable[tuple[type[QuerySet], str]] = ()
-    #
-    # @custom_filter
-    # def mapped_locally(self, queryset, value: bool, prefix: str) -> tuple[QuerySet, Q]:
-    #
-    #     if value not in (None, UNSET):
-    #         for queryset_class, method_name in self.mapped_locally_querysets:
-    #             if isinstance(queryset, queryset_class):
-    #                 queryset = getattr(queryset, method_name)(value)
-    #                 break
-    #         else:
-    #             raise Exception(f"Unexpected queryset class: {type(queryset)}")
-    #
-    #     return queryset, Q()
+class DependentMappedLocallyFilterMixin(AnnotationMergerMixin):
+    mapped_locally: Optional[bool]
+
+    def get_mapped_locally_querysets(self) -> Iterable[tuple[type[QuerySet], str]]:
+        raise NotImplementedError(
+            f"{self.__class__.__name__} must implement get_mapped_locally_querysets()",
+        )
+
+    @custom_filter
+    def mapped_locally(self, queryset, value: bool, prefix: str) -> tuple[QuerySet, Q]:
+
+        if value not in (None, UNSET):
+            for queryset_class, method_name in self.get_mapped_locally_querysets():
+                if isinstance(queryset, queryset_class):
+                    queryset = getattr(queryset, method_name)(value)
+                    break
+            else:
+                raise Exception(f"Unexpected queryset class: {type(queryset)}")
+
+        return queryset, Q()
 
 
 class GeneralMappedRemotelyFilterMixin(AnnotationMergerMixin):
