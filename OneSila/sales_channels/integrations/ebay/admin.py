1bd274db69e9d8f3014660769fb8ba17b1ac22a4
David Sebastian Bran <50082813+BranDavidSebastian@users.noreply.github.com>
2025-09-24 15:15:40 +0300

    Add admin for eBay category model

--- OneSila/sales_channels/integrations/ebay/admin.py
+++ OneSila/sales_channels/integrations/ebay/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import EbayCategory
+
+
+@admin.register(EbayCategory)
+class EbayCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "remote_id", "marketplace_default_tree_id")
+    list_filter = ("marketplace_default_tree_id",)
+    search_fields = ("name", "remote_id")
