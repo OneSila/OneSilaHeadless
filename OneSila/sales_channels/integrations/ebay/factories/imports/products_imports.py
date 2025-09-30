72a613ad531df1ff5e7e4b58764cf65c9fe644c7
david <brandavid1998@gmail.com>
2025-09-30 21:57:51 +0300

    cleanup code

--- OneSila/sales_channels/integrations/ebay/factories/imports/products_imports.py
+++ OneSila/sales_channels/integrations/ebay/factories/imports/products_imports.py
@@ -20,6 +20,7 @@ from imports_exports.helpers import append_broken_record
 from core.helpers import ensure_serializable
 from sales_channels.integrations.ebay.constants import EBAY_INTERNAL_PROPERTY_DEFAULTS
 from sales_channels.integrations.ebay.factories.mixins import GetEbayAPIMixin
+from sales_channels.integrations.ebay.helpers import get_is_product_variation
 from sales_channels.integrations.ebay.models import (
     EbayProduct,
     EbayInternalProperty,
@@ -33,45 +34,6 @@ from sales_channels.models import SalesChannelIntegrationPricelist
 from sales_prices.models import SalesPrice
 
 logger = logging.getLogger(__name__)
-
-
-def get_parent_skus(*, product_data: Any) -> set[str]:
-    """Return the parent SKU identifiers declared on the payload."""
-
-    if not isinstance(product_data, dict):
-        return set()
-
-    parent_skus: set[str] = set()
-
-    for key in ("group_ids", "inventory_item_group_keys"):
-        values = product_data.get(key)
-        if values is None:
-            continue
-
-        if isinstance(values, str) or not isinstance(values, (list, tuple, set)):
-            iterable = [values]
-        else:
-            iterable = list(values)
-
-        for value in iterable:
-            if value is None:
-                continue
-
-            normalized = value.strip() if isinstance(value, str) else str(value)
-            if not normalized:
-                continue
-
-            parent_skus.add(normalized)
-
-    return parent_skus
-
-
-def get_is_product_variation(*, product_data: Any) -> tuple[bool, set[str]]:
-    """Return whether the payload references a variation and its parent SKUs."""
-
-    parent_skus = get_parent_skus(product_data=product_data)
-
-    return (bool(parent_skus), parent_skus)
 
 
 class EbayProductsImportProcessor(ImportMixin, GetEbayAPIMixin):
