1fd50dca7a6ddff0102f2e730582fde8fe33b851
david <brandavid1998@gmail.com>
2025-10-16 13:08:28 +0300

    add ebay render template

--- OneSila/sales_channels/integrations/ebay/tests/tests_factories/test_product_content_factories.py
+++ OneSila/sales_channels/integrations/ebay/tests/tests_factories/test_product_content_factories.py
@@ -8,6 +8,7 @@ from sales_channels.integrations.ebay.factories.products.content import (
 from sales_channels.integrations.ebay.tests.tests_factories.mixins import (
     EbayProductPushFactoryTestBase,
 )
+from sales_channels.models.sales_channels import SalesChannelContentTemplate
 
 
 class EbayProductContentUpdateFactoryTest(EbayProductPushFactoryTestBase):
@@ -33,6 +34,45 @@ class EbayProductContentUpdateFactoryTest(EbayProductPushFactoryTestBase):
         api.sell_inventory_update_offer.assert_called_once_with(
             offer_id="OFFER-123",
             body={"listingDescription": "Listing description"},
+            content_language="en-us".replace("_", "-"),
+            content_type="application/json",
+        )
+
+    @patch(
+        "sales_channels.integrations.ebay.factories.products.mixins.EbayInventoryItemPayloadMixin._collect_image_urls",
+        return_value=["https://cdn.example.com/image.jpg"],
+    )
+    def test_updates_listing_description_with_content_template(self, _mock_collect_images):
+        SalesChannelContentTemplate.objects.create(
+            sales_channel=self.sales_channel,
+            language="en-us",
+            template="{{ content }} -- {{ title }}",
+        )
+
+        factory = EbayProductContentUpdateFactory(
+            sales_channel=self.sales_channel,
+            local_instance=self.product,
+            remote_product=self.remote_product,
+            view=self.view,
+            get_value_only=False,
+        )
+        payload = factory.build_inventory_payload()
+
+        expected_description = "Full description -- Test Product"
+        self.assertEqual(payload["product"]["description"], expected_description)
+
+        api = MagicMock()
+        factory.api = api
+        factory.update_remote()
+
+        api.sell_inventory_create_or_replace_inventory_item.assert_called_once()
+        create_call = api.sell_inventory_create_or_replace_inventory_item.call_args
+        create_payload = create_call.kwargs["body"]
+        self.assertEqual(create_payload["product"]["description"], expected_description)
+
+        api.sell_inventory_update_offer.assert_called_once_with(
+            offer_id="OFFER-123",
+            body={"listingDescription": expected_description},
             content_language="en-us".replace("_", "-"),
             content_type="application/json",
         )
