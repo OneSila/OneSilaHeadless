b247265b8bd6042a1aa289bf905366f266ea250d
david <brandavid1998@gmail.com>
2026-02-24 15:27:48 +0200

    # 611 remove code that breaks tests

--- OneSila/sales_channels/integrations/amazon/models/recommended_browse_nodes.py
+++ OneSila/sales_channels/integrations/amazon/models/recommended_browse_nodes.py
@@ -91,41 +91,43 @@ class AmazonProductBrowseNode(RemoteProductCategory):
     def __str__(self):
         return f"{self.product} @ {self.view}: {self.remote_id}"
 
-    def clean(self):
-        super().clean()
-
-        remote_id = (self.remote_id or "").strip()
-        if not remote_id:
-            return
-
-        categories = AmazonBrowseNode.objects.filter(remote_id=remote_id)
-
-        marketplace_id = None
-        if self.view_id:
-            if hasattr(self, "view") and self.view is not None:
-                marketplace_id = self.view.remote_id
-            else:
-                marketplace_id = SalesChannelView.objects.filter(
-                    id=self.view_id
-                ).values_list("remote_id", flat=True).first()
-
-        if marketplace_id:
-            categories = categories.filter(marketplace_id=marketplace_id)
-
-        try:
-            category = categories.get()
-        except AmazonBrowseNode.DoesNotExist as exc:
-            raise ValidationError(
-                {"remote_id": "Amazon browse node does not exist for the given remote ID."}
-            ) from exc
-        except AmazonBrowseNode.MultipleObjectsReturned as exc:
-            raise ValidationError(
-                {"remote_id": "Multiple Amazon browse nodes found for the given remote ID."}
-            ) from exc
-
-        if category.has_children:
-            raise ValidationError({"remote_id": "Only leaf Amazon browse nodes can be assigned."})
-
-    def save(self, *args, **kwargs):
-        self.full_clean()
-        return super().save(*args, **kwargs)
+    # @TODO: Come back at this later right now this break 80+ tests
+    # this was not initially here
+    # def clean(self):
+    #     super().clean()
+    #
+    #     remote_id = (self.remote_id or "").strip()
+    #     if not remote_id:
+    #         return
+    #
+    #     categories = AmazonBrowseNode.objects.filter(remote_id=remote_id)
+    #
+    #     marketplace_id = None
+    #     if self.view_id:
+    #         if hasattr(self, "view") and self.view is not None:
+    #             marketplace_id = self.view.remote_id
+    #         else:
+    #             marketplace_id = SalesChannelView.objects.filter(
+    #                 id=self.view_id
+    #             ).values_list("remote_id", flat=True).first()
+    #
+    #     if marketplace_id:
+    #         categories = categories.filter(marketplace_id=marketplace_id)
+    #
+    #     try:
+    #         category = categories.get()
+    #     except AmazonBrowseNode.DoesNotExist as exc:
+    #         raise ValidationError(
+    #             {"remote_id": "Amazon browse node does not exist for the given remote ID."}
+    #         ) from exc
+    #     except AmazonBrowseNode.MultipleObjectsReturned as exc:
+    #         raise ValidationError(
+    #             {"remote_id": "Multiple Amazon browse nodes found for the given remote ID."}
+    #         ) from exc
+    #
+    #     if category.has_children:
+    #         raise ValidationError({"remote_id": "Only leaf Amazon browse nodes can be assigned."})
+    #
+    # def save(self, *args, **kwargs):
+    #     self.full_clean()
+    #     return super().save(*args, **kwargs)
